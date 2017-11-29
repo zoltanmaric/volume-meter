6a49150297d832cf2191639bc5fdadee2069c719
zoltanmaric <zoltanmaric@protonmail.com>
2017-11-29 01:01:59 +0100

    create single horizontal volume meter, styled nicely

--- example/index.js
+++ example/index.js
@@ -1,32 +1,31 @@
 var volumemeter = require('../')
 var getusermedia = require('getusermedia')
 
-var left = document.querySelector('.left')
-var right = document.querySelector('.right')
+var progress = document.querySelector('#progress');
+var progressMax = document.querySelector('#progress-max');
+var progressText = document.querySelector('#progress-text');
+
+var maxLevel = 0;
 
 getusermedia({ audio: true, video: false }, function (err, stream) {
   if (err) return console.error(err)
 
   var ctx = new AudioContext
-  
-  var leftmeter = volumemeter(ctx, function (volume) {
-    left.style.height = volume + '%'
-  })
 
-  var rightmeter = volumemeter(ctx, function (volume) {
-    right.style.height = volume + '%'
-  })
-
-  var split = ctx.createChannelSplitter()
-  split.connect(leftmeter, 0, 0)
-  split.connect(rightmeter, 1, 0)
+  var meter = volumemeter(ctx, function (volume) {
+    progress.style.width = volume + '%';
+    if (volume > maxLevel) {
+      maxLevel = Math.round(volume);
+      progressText.textContent = maxLevel;
+      progressMax.style.width = volume + '%';
+    }
+  });
   
-  var src = ctx.createMediaStreamSource(stream)
-  src.connect(split)
-  src.connect(ctx.createGain())
+  var src = ctx.createMediaStreamSource(stream);
+  src.connect(meter);
+  src.connect(ctx.createGain());
 
   stream.onended = function () {
-    leftmeter.stop()
-    rightmeter.stop()
+    meter.stop()
   }
 })
